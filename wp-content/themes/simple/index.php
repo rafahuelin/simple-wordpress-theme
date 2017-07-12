e36fae8c273c1f54feecc7403cbf2ec7ceb88890
Rafa Huelin <it@engisa.com>
2017-07-12 14:51:45 +0100

    Add widget sidebar

--- wp-content/themes/simple/index.php
+++ wp-content/themes/simple/index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
-    <div class="main">
-        <div class="container">
+    <div class="container">
+        <div class="main">
             <?php if(have_posts()) :  ?>
                 <?php while(have_posts()): the_post(); ?>
                     <article class="post">
@@ -29,6 +29,14 @@
                 <?php echo wpautop('Sorry, No posts were found'); ?>
             <?php endif; ?>
         </div>
+
+        <div class="sidebar">
+            <?php if(is_active_sidebar('sidebar')) : ?>
+                <?php dynamic_sidebar('sidebar'); ?>
+            <?php endif; ?>
+        </div>
+
+        <div class="clr"></div>
     </div>
 
 <?php get_footer(); ?>
