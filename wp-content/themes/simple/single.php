889cab1d1ff4ea3576f6e89b348225eb9887c52b
Rafa Huelin <it@engisa.com>
2017-07-12 09:14:24 +0100

    Create single post page

--- wp-content/themes/simple/single.php
+++ wp-content/themes/simple/single.php
@@ -6,18 +6,12 @@
                 <?php while(have_posts()): the_post(); ?>
                     <article class="post">
                         <h3>
-                            <a href="<?php the_permalink(); ?>">
-                                <?php the_title(); ?>
-                            </a>
+                            <?php the_title(); ?>
                         </h3>
                         <div class="meta">
                             Created By <?php the_author(); ?> on <?php  the_time('F j, Y g:i a'); ?>
                         </div>
                         <?php the_content(); ?>
-                        <br>
-                        <a class="button" href="<?php the_permalink(); ?>">
-                            Read More
-                        </a>
                     </article>
                 <?php endwhile ?>
             <?php else : ?>
