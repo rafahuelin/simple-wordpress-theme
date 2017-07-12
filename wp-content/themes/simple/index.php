c0fe318d7537d7120447ec73d83d9d598127b8fd
Rafa Huelin <it@engisa.com>
2017-07-12 08:52:03 +0100

    Loop through posts

--- wp-content/themes/simple/index.php
+++ wp-content/themes/simple/index.php
@@ -7,6 +7,35 @@
     <?php wp_head(); ?>
 </head>
 <body>
+    <header>
+        <div class="container">
+            <h1><?php bloginfo('title'); ?></h1>
+            <span><?php bloginfo('description'); ?></span>
+        </div>
+    </header>
 
+    <div class="main">
+        <div class="container">
+            <?php if(have_posts()) :  ?>
+                <?php while(have_posts()): the_post(); ?>
+                    <h3><?php the_title(); ?></h3>
+                    <div class="meta">
+                        Created By <?php the_author(); ?> on <?php  the_time('F j, Y g:i a'); ?>
+                    </div>
+                    <?php the_content(); ?>
+                <?php endwhile ?>
+            <?php else : ?>
+                <?php echo wpautop('Sorry, No posts were found'); ?>
+            <?php endif; ?>
+        </div>
+    </div>
+
+    <footer>
+        <div class="container">
+            <p>&copy; <?php the_date('Y'); ?> &mdash; <?php bloginfo('name'); ?></p>
+        </div>
+    </footer>
+
+    <?php wp_footer(); ?>
 </body>
 </html>
